9c0663f7aa4e61dc763ce4bb5763ff1a3e223431
pvenegass <paulavenegassoto@gmail.com>
2024-08-16 21:48:40 -0400

    cambio1

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,17 @@
 num1 = 5
 num2 = -2
 
-def suma (numero1, numero2):
+def suma(numero1, numero2):
     total = numero1 + numero2
     return total
 
-
 resultado = suma(num1, num2)
 
-print('El resultado es:', resultado)
+
+def resta (numero1, numero2):
+    total = numero1 - numero2
+    return total
+
+resultado2 = resta(num1, num2)
+
+print('El resultado es:', resultado2)
